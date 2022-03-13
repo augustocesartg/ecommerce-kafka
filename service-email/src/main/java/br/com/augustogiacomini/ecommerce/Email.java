2e498c94543e4c5b1e62841f56903fe2ca771d47
augustogiacomini <augusto.giacomini@ciss.com.br>
2022-03-13 15:10:26 -0300

    first commit

---
+++ service-email/src/main/java/br/com/augustogiacomini/ecommerce/Email.java
@@ -0,0 +1,11 @@
+package br.com.augustogiacomini.ecommerce;
+
+public class Email {
+
+    private final String subject, body;
+
+    public Email(String subject, String body) {
+        this.subject = subject;
+        this.body = body;
+    }
+}
